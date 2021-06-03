33d87af91504345e70834265de04bef0746714f9
Eduardo Colabardini <eduardocolabardini@gmail.com>
2021-06-02 22:12:08 -0300

    Add Loconotion configuration files for my blog

---
+++ ecolabardini.github.io/google_analytics.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MJEEFDF8E8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MJEEFDF8E8');
+</script>
